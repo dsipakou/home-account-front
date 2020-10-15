e4b7f07ae02f9c89bee44483b711906af6c16c00
Denis Sipakov <mc.flyer@gmail.com>
2020-10-15 12:12:12 +0300

    Add material design

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Button from '@material-ui/core/Button';
 
 export const Home = () => {
   return (
-    <div>This is home</div>
+    <>
+      <div>This is home</div>
+      <Button variant="contained" color="primary">
+        Log In
+      </Button>
+    </>
   )
 }
