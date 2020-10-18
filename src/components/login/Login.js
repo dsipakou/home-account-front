d47ab643e6889cfd674001f6fb715c5a2e8c0e00
Denis Sipakov <mc.flyer@gmail.com>
2020-10-18 20:38:11 +0300

    Update

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { Container, Button } from '@material-ui/core';
+import { Container, Grid, Button, TextField } from '@material-ui/core';
 
 export const Login = () => {
   return (
-    <Container>
-      <Button variant="outlined" color="primary">
-        Log in here
-      </Button>
-    </Container>
+    <Grid
+      container
+      direction="column"
+      alignItems="left"
+    >
+      <Grid
+        container
+        item
+        xs={3}
+      >
+        <TextField color="primary" label="Username" />
+      </Grid>
+      <Grid
+        container
+        item
+        xs={3}
+      >
+        <TextField color="primary" label="Password" type="password" />
+      </Grid>
+      <Grid
+        container
+        item
+        xs={3}
+      >
+        <Button variant="outlined" color="primary">
+          Log in
+        </Button>
+      </Grid>
+    </Grid>
   )
 }
