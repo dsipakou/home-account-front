ac87d9e26ff3935b75fc432036f1132b7a8cc1d4
Denis Sipakov <mc.flyer@gmail.com>
2020-10-21 19:35:54 +0300

    Add toolbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import { Container, ButtonGroup, Button } from '@material-ui/core';
 import { Login, SignUp } from 'components';
 import { StyledContainer } from './styled';
 
 export const Header = () => {
+  const [newUser, setNewUser] = React.useState(false)
   return (
-    <StyledContainer>
-      <Login />
-    </StyledContainer>
+    <>
+      <StyledContainer>
+        <ButtonGroup variant="text">
+          <Button onClick={() => setNewUser(false)}>Login</Button>
+          <Button onClick={() => setNewUser(true)}>SignUp</Button>
+        </ButtonGroup>
+      </StyledContainer>
+
+      { newUser ? <SignUp /> : <Login /> }
+    </>
   )
 }
--- src/components/header/styled/index.js
+++ src/components/header/styled/index.js
@@ -3,5 +3,6 @@ import { Container } from '@material-ui/core';
 
 export const StyledContainer = styled(Container)`
   width: 100%;
+  height: 50px;
   background-color: rgb(141,114,54);
 `
