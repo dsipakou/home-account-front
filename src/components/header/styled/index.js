0ecaa7179f5b84f6bea2f76f5c0873d619b13d90
Denis Sipakov <mc.flyer@gmail.com>
2020-10-22 20:01:59 +0300

    Update styles

--- src/components/header/styled/index.js
+++ src/components/header/styled/index.js
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 import { Container } from '@material-ui/core';
 
 export const StyledContainer = styled(Container)`
+  display: flex;
+  flex-direction: row;
   width: 100%;
-  height: 50px;
+  height: 38px;
   background-color: rgb(141,114,54);
 `
