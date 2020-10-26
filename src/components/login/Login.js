40d5d1e3f8c0504299514da7281d4e3c06696247
Denis Sipakov <mc.flyer@gmail.com>
2020-10-26 18:39:54 +0300

    Update

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -8,31 +8,19 @@ export const Login = () => {
       direction="column"
       alignItems="left"
     >
-      <Grid
-        container
-        item
-        xs={3}
-      >
+      <Grid container item xs={3}>
         <TextField
           label="Email"
         />
       </Grid>
-      <Grid
-        container
-        item
-        xs={3}
-      >
+      <Grid container item xs={3}>
         <TextField
           label="Password"
           type="password"
           autoComplete="current-password"
         />
       </Grid>
-      <Grid
-        container
-        item
-        xs={3}
-      >
+      <Grid container item xs={3}>
         <Button variant="outlined" color="primary">
           Log in
         </Button>
