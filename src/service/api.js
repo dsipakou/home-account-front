4164c3c1df867f8190ec53bdbf86b1dc647d9bda
Denis Sipakov <mc.flyer@gmail.com>
2020-11-03 15:51:39 +0300

    Add login request

---
+++ src/service/api.js
@@ -0,0 +1,21 @@
+const IP_ADDRESS = '127.0.0.1';
+const PORT = '1010';
+const HOST = `http://${IP_ADDRESS}:${PORT}`
+
+const registerUrl = `${HOST}/api/users/register`;
+const loginUrl = `${HOST}/api/users/login`;
+const logoutUrl = `${HOST}/api/users/logout`;
+
+const body = (method, data) => {
+  return {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  };
+};
+
+export const doLogin = (email, password) => {
+  fetch(loginUrl, body('POST', {email, password}).then((data) => data));
+}
