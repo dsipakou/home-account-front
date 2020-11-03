21f806339592cd7e7830513d29c3ac676b0d975b
Denis Sipakov <mc.flyer@gmail.com>
2020-11-03 18:13:21 +0300

    Update login to read input values

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Container, Grid, Button, TextField } from '@material-ui/core';
 
 export const Login = () => {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const doLogin = (event) => {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+    console.log(`User logged in with: ${email.value} and ${password.value}`);
+  }
+
   return (
-    <Grid
-      container
-      direction="column"
-      alignItems="left"
-    >
-      <Grid container item xs={3}>
-        <TextField
-          label="Email"
-        />
+    <form onSubmit={doLogin}>
+      <Grid container direction="column" alignItems="left">
+        <Grid container item xs={3}>
+          <TextField
+            name="email"
+            label="Email"
+          />
+        </Grid>
+        <Grid container item xs={3}>
+          <TextField
+            name="password"
+            ref={passwordRef}
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+          />
+        </Grid>
+        <Grid container item xs={3}>
+          <Button type="submit" variant="outlined" color="primary" >
+            Log in
+          </Button>
+        </Grid>
       </Grid>
-      <Grid container item xs={3}>
-        <TextField
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-      </Grid>
-      <Grid container item xs={3}>
-        <Button variant="outlined" color="primary">
-          Log in
-        </Button>
-      </Grid>
-    </Grid>
+    </form>
   )
 }
