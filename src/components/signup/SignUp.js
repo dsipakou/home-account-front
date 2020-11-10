b8837dd28bbe3ad731cf2841fa7b426cb5d9607c
Denis Sipakov <mc.flyer@gmail.com>
2020-11-10 18:01:53 +0300

    Reformat

--- src/components/signup/SignUp.js
+++ src/components/signup/SignUp.js
@@ -3,36 +3,17 @@ import { Grid, Button, TextField } from '@material-ui/core';
 
 export const SignUp = () => {
   return (
-    <Grid
-      container
-      direction="column"
-      alignItems="left"
-    >
-      <Grid
-        container
-        item
-        xs={3}
-      >
+    <Grid container direction="column" alignItems="left">
+      <Grid container item xs={3}>
         <TextField label="Email" />
       </Grid>
-      <Grid
-        container
-        item
-        xs={3}
-      >
+      <Grid container item xs={3}>
         <TextField label="Password" type="password" />
       </Grid>
-      <Grid
-        container
-        item
-        xs={3}
-      >
+      <Grid container item xs={3}>
         <TextField label="Repeat password" type="password"/>
       </Grid>
-      <Grid
-        container
-        item
-      >
+      <Grid container item>
         <Button variant="contained" color="primary">
           Sign Up
         </Button>
