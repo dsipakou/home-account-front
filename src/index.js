5454813a376022e7e8a93b1824eeae9176ac18e3
Denis Sipakov <mc.flyer@gmail.com>
2020-11-25 20:59:21 +0300

    Add react-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Home } from './components/Home'
+import { ReactRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </Router>
   )
 }
 ReactDOM.render(<App />, document.getElementById('root'))
