50e78d20d969d7fa649a7e66211f16d9f3482ae2
Denis Sipakov <mc.flyer@gmail.com>
2021-01-26 16:19:53 +0300

    Add context auth

--- src/components/transactions/new/NewTransaction.js
+++ src/components/transactions/new/NewTransaction.js
@@ -9,27 +9,27 @@ export const NewTransaction = () => {
   }
 
   return (
-      <form onSubmit={addTransaction}>
-        <Grid container direction="column">
-          <Grid container item xs={3}>
-            <TextField
-              name="name"
-              label="Transaction Name"
-            />
-          </Grid>
-          <Grid container item xs={3}>
-            <TextField
-              name="value"
-              ref={valueRef}
-              label="Amount"
-            />
-          </Grid>
-          <Grid>
-            <Button type="submit" variant="outlined" color="primary">
-              Save
-            </Button>
-          </Grid>
+    <form onSubmit={addTransaction}>
+      <Grid container direction="column">
+        <Grid container item xs={3}>
+          <TextField
+            name="name"
+            label="Transaction Name"
+          />
         </Grid>
-      </form>
+        <Grid container item xs={3}>
+          <TextField
+            name="value"
+            ref={valueRef}
+            label="Amount"
+          />
+        </Grid>
+        <Grid>
+          <Button type="submit" variant="outlined" color="primary">
+            Save
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,25 +5,28 @@ import { Header } from 'components/header';
 import { Account, Login, SignUp} from 'components/user';
 import { Transactions, NewTransaction } from 'components/transactions';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AuthContext } from 'context/context';
 import './index.css';
 
 import { Container } from '@material-ui/core';
 
 const App = () => {
   return (
-    <Container>
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/transactions' component={Transactions} />
-          <Route path='/login' component={Login} />
-          <Route path='/signup' component={SignUp} />
-          <Route path='/account' component={Account} />
-          <Route path='/transactions/add' component={NewTransaction} />
-        </Switch>
-      </Router>
-    </Container>
+    <AuthContext.Provider>
+        <Container>
+          <Router>
+            <Header />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/transactions' component={Transactions} />
+              <Route path='/login' component={Login} />
+              <Route path='/signup' component={SignUp} />
+              <Route path='/account' component={Account} />
+              <Route path='/transactions/add' component={NewTransaction} />
+            </Switch>
+          </Router>
+        </Container>
+    </AuthContext.Provider>
   )
 }
 ReactDOM.render(<App />, document.getElementById('root'))
